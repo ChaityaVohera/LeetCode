e4321b68fdc5b61c7c54a85842fd1332c56f8f6d
Chaitya Vohera <56110415+ChaityaVohera@users.noreply.github.com>
2024-06-12 22:17:53 +0530

    Time: 3 ms (88.48%), Space: 44.3 MB (47.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,22 +4,16 @@ class Solution {
         if(s.length()!= t.length())
             return false;
         
-        int[] count = new int[26];
+        // Convert strings to character arrays
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
         
-        for(int i=0 ; i<s.length() ; i++){
-            count[s.charAt(i) - 'a']++;
-        }
+        // Sort the character arrays
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
         
-         for(int i=0 ; i<t.length() ; i++){
-            count[t.charAt(i) - 'a']--;
-        }
-        
-        for(int i=0 ; i<count.length ; i++){
-            if(count[i] != 0){
-                return false;
-            }
-        }
-        return true;
+        // Compare sorted arrays to check if they are equal
+        return Arrays.equals(sArray, tArray);
     }
 }   
 
